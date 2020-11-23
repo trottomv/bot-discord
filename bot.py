e1dae3071c036f759bd8635561b1fe35a383f1cd
Matteo Vitali <trottomv@gmail.com>
2020-11-23 23:24:07 +0100

    Add commands

--- bot.py
+++ bot.py
@@ -4,46 +4,56 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv("DISCORD_GUILD")
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="./")
+
+
+@bot.command(name="ping")
+async def ping(ctx):
+    await ctx.channel.send("pong")
+
+
+@bot.command()
+async def parrot(ctx, *, arg):
+    await ctx.send(arg)
 
 
 class Guild:
     def chat_server():
         """Return the single server instance from environment settings."""
-        return discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+        return discord.utils.find(lambda g: g.name == GUILD, bot.guilds)
 
 
-@client.event
+@bot.event
 async def on_ready():
-    """Run bot client."""
+    """Run bot bot."""
     guild = Guild.chat_server()
+    print(f"{bot.user} is connected to the following guild:\n" f"{guild.name}")
 
-    print(f"{client.user} is connected to the following guild:\n" f"{guild.name}")
 
-
-@client.event
+@bot.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(f"Hi {member.name}, welcome to {GUILD}!")
 
 
-@client.event
+@bot.add_listener
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
-    happy_birthday_message = "Auguri di Buon Compleanno"
+    greetings_message = "Ciao"
 
-    if "buon compleanno" in message.content.lower() and message.raw_mentions:
+    if "ciao" in message.content.lower():
         await message.channel.send(
-            f"{happy_birthday_message} <@!{message.raw_mentions[0]}> ! :cake: :champagne_glass:"
+            f"{greetings_message} <@!{message.author.id}> ! :wave:"
         )
 
 
 if __name__ == "__main__":
-    client.run(TOKEN)
+    bot.run(TOKEN)
